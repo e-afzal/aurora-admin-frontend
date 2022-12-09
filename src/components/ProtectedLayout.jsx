7e58a8f48037584c01fa1e60a318f8e2a26000f1
Essam Afzal <essam.afzal@outlook.com>
2022-12-10 02:34:38 +0400

    redirect if auth error

--- src/components/ProtectedLayout.jsx
+++ src/components/ProtectedLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import Login from '../pages/login';
 
 const ProtectedLayout = ({ children }) => {
   const { isLoading, error, isAuthenticated } = useAuth0();
   return (
     <>
-      {error || !isAuthenticated && !isLoading && <p>Authentication error..</p>}
+      {error || !isAuthenticated && !isLoading && <Login />}
       {!error && isLoading && <p>Loading...</p>}
       {!error && !isLoading && isAuthenticated && (
         <>
